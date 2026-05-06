b0ec7f4b2898c61e56e58651e08450bc33841440
kennedykariuki <kennedymaina657@gmail.com>
2026-05-06 11:58:46 +0300

    Styling and made base.html
    i styled using bootstrap and made base.html

--- Blog Page/the_blog/my_blog/urls.py
+++ Blog Page/the_blog/my_blog/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     # path("", views.index, name="index")
-    path("", HomeView.as_view(), name="Home"),
+    path("", HomeView.as_view(), name="home"),
     path("article/<int:pk>", ArticleDetailsView.as_view(), name="article_details")
 ]
